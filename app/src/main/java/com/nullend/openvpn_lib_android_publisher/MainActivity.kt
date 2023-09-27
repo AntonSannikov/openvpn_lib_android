f3018decfaaf05bd580a8083c366dac49f83216c
AntonSannikov <w.asas@list.ru>
2023-09-27 10:46:20 +0700

    initial

---
+++ app/src/main/java/com/nullend/openvpn_lib_android_publisher/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.nullend.openvpn_lib_android_publisher
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nullend.openvpn_lib_android_publisher.ui.theme.Openvpn_lib_android_publisherTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Openvpn_lib_android_publisherTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Openvpn_lib_android_publisherTheme {
+        Greeting("Android")
+    }
+}
